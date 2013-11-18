02b7ec48b1f283d6ed9c4afdfd9fed56041018bd
David Tudury <david.tudury@voxer.com>
2013-11-17 18:34:51 -0800

    better handle redraw on resize issue

--- examples/simple.js
+++ examples/simple.js
@@ -5,13 +5,11 @@ var ansi = require('../lib/ansi');//termly.ansi;
 var events = require('../lib/events');//termly.events;
 var buffer = require('../lib/buffer');//termly.buffer;
 
-var bottom = terminal.rows - 1;
-var top = bottom;
 var cursor = 0;
 var x = 0;
 var y = 0;
-var message = "";
-var miniBuffer = [];
+var miniBuffers = [];
+enter();
 
 termly.init();
 termly.dispatcher.on(termly.ansi.keys.CTRL_C, function () {
@@ -27,8 +25,10 @@ dispatcher.on(ansi.keys.UP, up);
 dispatcher.on(ansi.keys.DOWN, down);
 dispatcher.on(ansi.keys.SPACE, append);
 dispatcher.on(ansi.keys.BACKSPACE, backspace);
+dispatcher.on(ansi.keys.ENTER, enter);
 dispatcher.on(ansi.keys.DEL, del);
 dispatcher.on(events.terminal.NORMAL_KEY, append);
+dispatcher.on(events.terminal.RESIZE, handleResize);
 redraw();
 
 function left() {
@@ -40,62 +40,56 @@ function right() {
     updateCursor();
 }
 function down() {
-    cursor += miniBuffer[y].length;
+    cursor += miniBuffers[0].lines[y].length;
     updateCursor();
 }
 function up() {
-    if(y > 0) {
-        cursor -= miniBuffer[y - 1].length;
-    } else {
-        cursor -= miniBuffer[y].length;
-    }
+    cursor -= miniBuffers[0].lines[Math.max(0, y - 1)].length;
     updateCursor();
 }
 function append(data) {
-    message = message.substring(0, cursor) + data + message.substring(cursor);
+    var color = ansi.text.setBackground(Math.floor(Math.random() * 8));
+    miniBuffers[0].spliceMessage(cursor, 0, color + data);
     cursor += data.length;
     redraw();
 }
 function backspace() {
-    message = message.substring(0, cursor - 1) + message.substring(cursor);
+    miniBuffers[0].spliceMessage(cursor - 1, 1);
     cursor--;
     redraw();
 }
 function del() {
-    message = message.substring(0, cursor) + message.substring(cursor + 1);
+    miniBuffers[0].spliceMessage(cursor, 1);
+    redraw();
+}
+function enter() {
+    miniBuffers.unshift(new buffer.miniBuffer());
     redraw();
 }
 
+function handleResize() {
+    redraw(true);
+}
 
-function redraw() {
-    miniBuffer = [];
-    var messageCopy = message.slice();
-    while(messageCopy.length) {
-        miniBuffer.push(messageCopy.substring(0, terminal.columns));
-        messageCopy = messageCopy.substring(terminal.columns);
+function redraw(force) {
+    buffer.clear();
+    var bottomSoFar = terminal.rows - 1;
+    for(var i = 0; i < miniBuffers.length && bottomSoFar > 0; i++) {
+        miniBuffers[i].bottom(bottomSoFar);
+        miniBuffers[i].render();
+        bottomSoFar = miniBuffers[i].top() - 1;
     }
-    var lastLine = miniBuffer[miniBuffer.length - 1];
-    if(!lastLine || lastLine.length === terminal.columns) {
-        miniBuffer.push("");
-    }
-    top = bottom - (miniBuffer.length - 1);
-    for(var y = 0; y < miniBuffer.length; y++) {
-        buffer.setLine(y + top, miniBuffer[y]);
-    }
-    buffer.draw();
+//    buffer.writeAt(1, 1, "size: " + terminal.columns + "x" + terminal.rows);
+    buffer.draw(force);
     updateCursor();
 }
 
 function updateCursor() {
-    cursor = Math.max(0, Math.min(message.length, cursor));
+    var lines = miniBuffers[0].lines;
+    cursor = Math.max(0, Math.min(miniBuffers[0].messageLength(), cursor));
     x = cursor;
-    y = 0;
-    for(y = 0; y < miniBuffer.length && x > miniBuffer[y].length; y++) {
-        x -= miniBuffer[y].length;
+    for(y = 0; y < lines.length && (x > lines[y].length || x === terminal.columns); y++) {
+        x -= lines[y].length;
     }
-    if(x === terminal.columns) {
-        y++;
-        x = 0;
-    }
-    terminal.moveTo(x + 1, top + y + 1);
+    terminal.moveTo(x + 1, miniBuffers[0].top() + y + 1);
 }
